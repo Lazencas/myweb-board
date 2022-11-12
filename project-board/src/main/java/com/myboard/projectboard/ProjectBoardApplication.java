20c56bcba9197ef2a170844ee09ec937d9fb374e
GunTae Kim <57083072+Lazencas@users.noreply.github.com>
2022-11-13 02:07:04 +0900

    Update ProjectBoardApplication.java

--- project-board/src/main/java/com/myboard/projectboard/ProjectBoardApplication.java
+++ project-board/src/main/java/com/myboard/projectboard/ProjectBoardApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjectBoardApplication {
 
-	public static void main(String[] args) {
-		system.out.print("hello")
+	public static void main(String[] args) {		
 		SpringApplication.run(ProjectBoardApplication.class, args);
 	}
 
