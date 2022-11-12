d1acc5f04a2301d59122e2f047f002d09cad5629
Guntae Kim <dkfkskgks@naver.com>
2022-11-13 01:41:49 +0900

    #8 - 도메인 클래스 설계(게시글, 댓글)
    도메인 정보를 설계 함
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와의 연동과정에서 추가적으로 업데이트 될 수 있음

---
+++ project-board/src/main/java/com/myboard/projectboard/ProjectBoardApplication.java
@@ -0,0 +1,14 @@
+package com.myboard.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoardApplication {
+
+	public static void main(String[] args) {
+		system.out.print("hello")
+		SpringApplication.run(ProjectBoardApplication.class, args);
+	}
+
+}
